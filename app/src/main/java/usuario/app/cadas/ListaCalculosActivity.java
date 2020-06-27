0eb4bfed4252d6d9aaa71ffe96d1d8e7b3ec6084
Pedro Barros <pedrokatsbarros@gmail.com>
2020-06-26 21:07:58 -0300

    Retiradas mensagens de sucesso de conexão com o BD

--- app/src/main/java/usuario/app/cadas/ListaCalculosActivity.java
+++ app/src/main/java/usuario/app/cadas/ListaCalculosActivity.java
@@ -73,13 +73,11 @@ public class ListaCalculosActivity extends AppCompatActivity {
             dadosOpenHelper = new DadosOpenHelper(this); //Lembre-se que nosso contrutor pede o contexto, ou seja, a Activity em questão
             //Criando conexão
             conexao = dadosOpenHelper.getWritableDatabase(); //O writable permite leitura e escrita, enquanto q o redtable, só leitura
-            Toast mensagemConfirmacao = Toast.makeText(this, "Conexão estabelecida com sucesso!", Toast.LENGTH_SHORT);
-            mensagemConfirmacao.show();
 
         }catch (SQLException ex){ //Há dois SQLException e temos q usar o android.database, pois é o q pertence ao pacote do SQLite
-            Toast mensagemErro = Toast.makeText(this, "Erro! Não foi possível se conectar com o banco de dados.", Toast.LENGTH_SHORT);
+            Toast mensagemErro = Toast.makeText(this, R.string.erro_conexao_bd, Toast.LENGTH_SHORT);
             mensagemErro.show();
-            finish();
+            finishAffinity(); //Fecha a activity atual e todas as que estão abaixo na pilha (abertas anteriormente)
         }
     }
 }
